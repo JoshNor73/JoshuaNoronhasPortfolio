f8736269704e3fc502ba4fb1d3cb698ba3c91895
JoshNor73 <joshuanoronha73@gmail.com>
2024-03-12 15:45:37 -0300

    Add window-resize responsiveness

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function redirectWeb(x) {
     window.open("https://" + x, '_blank')?.focus();
 }
 
+function resizeAction() {
+    if(window.outerWidth <= 1000) {
+        console.log("smol");
+        var m = document.getElementById("main");
+    } else {
+        console.log("BIG");
+    }
+}
